a517dd1a6058c0ecc4ba515a8cc12779dc4b8114
sudarsonhalder <sudarsonhalder31@gmail.com>
2020-12-08 18:34:02 +0530

    update

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
+import test from "../src/images/test.png";
 const Board=()=>{
     return(
-   <>Hello</>
+   <>      <h2  className='brand-name2'><strong className='brand-name'>State Boards</strong></h2>
+   <div class="row">
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test} class="cardimg3 " alt="Maharashtra"></img>
+       <p class="paracard5">Maharashtra</p>
+   </div>
+   </div>
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="Rajasthan"></img>
+       <p class="paracard5">Rajasthan</p>
+   </div>
+   </div>
+   
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="Madhya Pradesh"></img>
+       <p class="paracard5">Madhya Pradesh</p>
+   </div>
+   </div>
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="West Bengal"></img>
+       <p class="paracard5">West Bengal</p>
+   </div>
+   </div>
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="Gujrat"></img>
+       <p class="paracard5">Gujrat</p>
+   </div>
+   </div>
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="Uttar Pradesh"></img>
+       <p class="paracard5">Uttar Pradesh</p>
+   </div>
+   </div>
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="Kerala"></img>
+       <p class="paracard5">Kerala</p>
+   </div>
+   </div>
+   <div class=" col-md-3 ">
+       <div class="maincard5">
+       <img src={test}  class="cardimg " alt="Tamil Nadu"></img>
+       <p class="paracard5">Tamil Nadu</p>
+   </div>
+   </div>
+   </div>
+   </>
 
     );
 };
--- src/Exam.jsx
+++ src/Exam.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import examprep from "./images/examprep.png";
 const Exam=()=>{
-    return(
-   <>Hello</>
-
+    return(<>
+        <section id='header' className='d-flex align-items-center'>
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-10 mx-auto mt-0'>
+                <h2 className='brand-name2'><strong className='brand-name'>JEE</strong> Mains and Advanced Preparation.</h2>
+                    <div className='row'>
+                    <div className='col-md-5 pt-5 pt-lg-0 order-1 order-lg-0 s-flex justify-content-center flex-column'>
+                   <h3 className='brand-name'>Micro concept videos</h3>
+                   <h6>Find Multilingual video explantion important and frequently confused concepts from every subjects</h6>
+                    </div>
+                    <div className='col-lg-6 order-1 order-lg-2 header-img'>
+                    <img src={examprep} className='img-fluid animated' alt='Home img'/>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+         <section id='header' className='d-flex align-items-center'>
+         <div className='container-fluid'>
+             <div className='row'>
+                 <div className='col-10 mx-auto mt-0'>
+                 <h2 className='brand-name2'><strong className='brand-name'>NEET</strong> Preparation.</h2>
+                     <div className='row'>
+                     <div className='col-md-5 pt-5 pt-lg-0 order-1 order-lg-0 s-flex justify-content-center flex-column'>
+                    <h3 className='brand-name'>Micro concept videos</h3>
+                    <h6>Find Multilingual video explantion important and frequently confused concepts from every subjects</h6>
+                     </div>
+                     <div className='col-lg-6 order-1 order-lg-2 header-img'>
+                     <img src={examprep} className='img-fluid animated' alt='Home img'/>
+                     </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+         </section>
+        
+</>
     );
 };
 
